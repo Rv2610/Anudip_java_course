e383aa99f661bf88314ae31933ad45f0d249b817
Rv2610 <90789093+Rv2610@users.noreply.github.com>
2022-06-24 12:04:14 +0530

    Update NumberIsPrimeOrNot.java

--- Lab/NumberIsPrimeOrNot.java
+++ Lab/NumberIsPrimeOrNot.java
@@ -3,20 +3,22 @@ package ROHIT;
 public class NumberIsPrimeOrNot {
 
 	public static void main(String[] args) {
-		int number = 1;
-		if(number == 1) {
-			System.out.println("not a prime number");
-		}
-		else if(number == 2) {
-			System.out.println("it is a prime number");
-		}
-		else if(number % 2 != 0) {
-			System.out.println("it is a prime number");
-		}
-		else {
-			System.out.println("not a prime number");
-		}
+		int num = 15;
+		int s = 0;
+		for(int i = 2 ; i<= num/2 ; i++){
+			if(num%i ==0){
+				s = 1;
+				break;
+			}
+			if(s == 1){
+				System.out.println(num + " is a prime number");
+			}
+			else{
+				System.out.println(num + " is not prime");
+			}
 
+		}
 	}
 
 }
+
