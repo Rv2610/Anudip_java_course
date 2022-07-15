9012d9551905c11748c04523e53693116951d714
Rv2610 <90789093+Rv2610@users.noreply.github.com>
2022-07-15 11:43:26 +0530

    Create Circle.java

---
+++ Practical/Circle.java
@@ -0,0 +1,34 @@
+/*
+ * author : rohit rathod
+ */
+
+package ROHIT;
+
+public class Circle {
+	
+	float radius;
+
+	
+	
+	public Circle() {
+		
+		radius = 1.5f;
+		System.out.println("this is the new radius given = "+ radius);
+	}
+
+}
+
+
+class Shape {
+
+	public static void main(String[] args) {
+		
+		Circle c = new Circle();
+		
+		
+		
+
+	}
+
+}
+
